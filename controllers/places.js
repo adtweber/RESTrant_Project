3200749441391af477f886392df24127c684c5ff
Andi Weber <adtweber@gmail.com>
2024-05-03 19:15:09 -0400

    added edit form

--- controllers/places.js
+++ controllers/places.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const places = require("../models/places.js")
 
 router.get('/', (req, res) => {
-    res.render('places/index', {places})
+    res.render('places/index', { places })
 })
 
 router.get('/new', (req, res) => {
@@ -20,6 +20,10 @@ router.get('/:id', (req, res) => {
     else {
         res.render('places/show', { place: places[id], id })
     }
+})``
+
+router.put('/:id', (req, res) => {
+    res.send('PUT /places/:id stub')
 })
 
 router.get('/:id', (req, res) => {
@@ -40,6 +44,19 @@ router.post('/', (req, res) => {
     }
     places.push(req.body)
     res.redirect('/places')
+})
+
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id], id: id })
+    }
 })
 
 router.delete('/:id', (req, res) => {
