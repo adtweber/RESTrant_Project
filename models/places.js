2291974f12d9b1cc9365aa79899833293168c52f
Andi Weber <adtweber@gmail.com>
2024-05-03 19:47:26 -0400

    installed mongo

--- models/places.js
+++ models/places.js
@@ -1,10 +1,24 @@
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	pic: String,
+	cuisines: { type: String, required: true },
+	city: { type: String, default: 'Anytown' },
+	state: { type: String, default: 'USA' },
+	founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
 places = [{
 	name: 'H-Thai-ML',
 	city: 'Seattle',
 	state: 'WA',
 	cuisines: 'Thai, Pan-Asian',
 	pic: '/img/h-thai-ml-tables.jpg'
-    }, {
+}, {
 	name: 'Coding Cat Cafe',
 	city: 'Phoenix',
 	state: 'AZ',
