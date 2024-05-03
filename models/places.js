2d71f3bf9925f16d1010b6eb53ca348a33c4994b
Andi Weber <adtweber@gmail.com>
2024-05-03 19:56:02 -0400

    edited models/places.js

--- models/places.js
+++ models/places.js
@@ -7,36 +7,6 @@ const placeSchema = new mongoose.Schema({
 	city: { type: String, default: 'Anytown' },
 	state: { type: String, default: 'USA' },
 	founded: Number
-})
+});
 
-module.exports = mongoose.model('Place', placeSchema)
-
-
-places = [{
-	name: 'H-Thai-ML',
-	city: 'Seattle',
-	state: 'WA',
-	cuisines: 'Thai, Pan-Asian',
-	pic: '/img/h-thai-ml-tables.jpg'
-}, {
-	name: 'Coding Cat Cafe',
-	city: 'Phoenix',
-	state: 'AZ',
-	cuisines: 'Coffee, Bakery',
-	pic: '/img/coffee-cat.jpg'
-}]
-
-module.exports = places;
-
-// const mongoose = require('mongoose')
-
-// const placeSchema = new mongoose.Schema({
-// 	name: { type: String, required: true },
-// 	pic: String,
-// 	cuisines: { type: String, required: true },
-// 	city: { type: String, default: 'Anytown' },
-// 	state: { type: String, default: 'USA' },
-// 	founded: Number
-// });
-
-// module.exports = mongoose.model('Place', placeSchema);
+module.exports = mongoose.model('Place', placeSchema);
