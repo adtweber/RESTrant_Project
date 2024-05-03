636d83fdda102c57dcdd53ad4c5dd22bc28c0b8e
Andi Weber <adtweber@gmail.com>
2024-05-03 19:27:52 -0400

    added edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const places = require("../models/places.js")
+const places = require("../models/places")
 
 router.get('/', (req, res) => {
     res.render('places/index', { places })
@@ -18,12 +18,8 @@ router.get('/:id', (req, res) => {
         res.render('error404')
     }
     else {
-        res.render('places/show', { place: places[id], id })
+        res.render('places/show', { place: places[id], id: id })
     }
-})``
-
-router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
 })
 
 router.get('/:id', (req, res) => {
@@ -34,7 +30,7 @@ router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
         // Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/200/300'
+        req.body.pic = 'http://placekitten.com/400/400'
     }
     if (!req.body.city) {
         req.body.city = 'Anytown'
@@ -71,6 +67,34 @@ router.delete('/:id', (req, res) => {
     else {
         places.splice(id, 1)
         res.redirect('/places')
+    }
+})
+
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
     }
 })
 
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,33 +1,55 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
-function edit_form(data) {
+function edit_form({ place, id }) {
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${id}?_method=PUT`} >
                     <div className="row">
-                        <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" value={data.place.name} required />
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="name">Place Name</label>
+                            <input type="text"
+                                name="name"
+                                id="name"
+                                required
+                                defaultValue={place.name} />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place City</label>
+                            <input id="city" type="text"
+                                name="city"
+                                required
+                                defaultValue={place.city} />
+                        </div>
                     </div>
-                    <div className="form-group col-sm-6">
-                        <label htmlFor="pic">Place Picture</label>
-                        <input className="form-control" id="pic" name="pic" />
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place State</label>
+                            <input id="state" type="text"
+                                name="state"
+                                required
+                                defaultValue={place.state} />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place Picture</label>
+                            <input id="pic" type="text"
+                                name="pic"
+                                required
+                                defaultValue={place.pic} />
+                        </div>
                     </div>
-                    <div className="form-group col-sm-6">
-                        <label htmlFor="city">City</label>
-                        <input className="form-control" id="city" name="city" />
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place Cuisines</label>
+                            <input id="cuisines" type="text"
+                                name="cuisines"
+                                required
+                                defaultValue={place.cuisines} />
+                        </div>
                     </div>
-                    <div className="form-group col-sm-6">
-                        <label htmlFor="state">State</label>
-                        <input className="form-control" id="state" name="state" />
-                    </div>
-                    <div className="form-group col-sm-6">
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control" id="cuisines" name="cuisines" required />
-                    </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                    <input type="submit" />
                 </form>
             </main>
         </Def>
